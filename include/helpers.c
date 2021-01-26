bd70e534e4b74b59e9c7be468ff30cd36a908c60
Unai <unaihtc70@gmail.com>
2021-01-26 14:07:39 +0100

    Helpers added
    - semaphores added to helpers

--- include/helpers.c
+++ include/helpers.c
@@ -12,3 +12,55 @@ void print_info(char *str, int arrow)
         printf(" => INFO: %s\n", str);
     }
 }
+
+void init_sems()
+{
+    int flag_error = 0;
+    if (sem_init(&timer_sem, 0, 0) == -1)
+    {
+        print_info("No se puede crear timer_sem", 1);
+        flag_error = 1;
+    }
+
+    if (sem_init(&cpu_sem, 0, 1) == -1)
+    {
+        print_info("No se puede crear cpu_sem", 1);
+        flag_error = 1;
+    }
+
+    if (sem_init(&ram_sem, 0, 1) == -1)
+    {
+        print_info("No se puede crear ram_sem", 1);
+        flag_error = 1;
+    }
+
+    if (sem_init(&pageT_sem, 0, 1) == -1)
+    {
+        print_info("No se puede crear oageT_sem", 1);
+        flag_error = 1;
+    }
+
+    if (sem_init(&q_sem, 0, 1) == -1)
+    {
+        print_info("No se puede crear q_sem", 1);
+        flag_error = 1;
+    }
+
+    if (sem_init(&oQ_sem, 0, 1) == -1)
+    {
+        print_info("No se puede crear oQ_sem", 1);
+        flag_error = 1;
+    }
+
+    if (flag_error == 1) exit(-1);
+}
+
+void sem_up_t(sem_t *opt)
+{
+    if (sem_post(opt) == -1) print_info("Error: sem_post(opt)", 1);
+}
+
+void sem_down_t(sem_t *opt)
+{
+    sem_wait(opt);
+}
--- include/helpers.h
+++ include/helpers.h
@@ -1,6 +1,33 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+#include <semaphore.h>
+
+/**
+ * Definicion de todos los semaforos necesarios.
+ **/
+sem_t timer_sem, cpu_sem, ram_sem, pageT_sem, q_sem, oQ_sem;
+
+/**
+ * Funcion para imprimir info por pantalla
+ **/
 void print_info(char *str, int arrow);
+
+/**
+ * Funcion que inicializa los semaforos
+ **/
+void init_sems();
+
+/**
+ * Funcion que levanta los semaforos, necesaria porque puede
+ * haber problemas a la hora de levantarlos.
+ **/
+void sem_up_t(sem_t *opt);
+
+/**
+ * Funcion para bajar los semaforos, no es necesaria pero se
+ * ha incluido por claridad.
+ **/
+void sem_down_t(sem_t *opt);
 
 #endif

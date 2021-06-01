ce80dbae26d2756de360b759d00c1aa5111adcfd
Unai <unaihtc70@gmail.com>
2021-06-01 20:15:47 +0200

    Add new_program from file.

--- workers/process_gen.c
+++ workers/process_gen.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include "../include/queue.h"
 #include "../include/task.h"
+#include "../include/pgb.h"
+#include "../include/ram.h"
 
 extern sem_t queue_access;
 
@@ -25,28 +28,62 @@ void generate_task(Task *t)
     gettimeofday(&t->start_time, 0);
 }
 
+void new_program(Task *t, const char *file)
+{
+    int lines, pages, aloc;
+
+    FILE *f = fopen(file, "r");
+    if (f != NULL)
+    {
+        fseek(f, 0, SEEK_END);
+        int size = ftell(f);
+
+        Word *rom = malloc(size);
+        fseek(f, 0, SEEK_SET);
+        fread(rom, size, 1, size); // leer la rom
+        fclose(f);
+
+        generate_task(t);
+        
+        // Calcular direcciones de .text y .data.
+        // Tambien asignamos las direcciones.
+        t->mm.code = (rom[5] << 2) + (rom[6] << 1) + rom[7];
+        t->mm.data = (rom[13] << 2) + (rom[14] << 1) + rom[15];
+
+        // Calcular numero de lineas (se usa luego) y de paginas.
+        lines = size - 16;
+        pages = (lines / (int) PAGESIZE) + ((lines % (int) PAGESIZE > 0) ? 1 : 0); // TODO: evitar el modulo???
+
+        Pages pg;
+
+        for (int i = 0; i < pages; i++)
+        {
+            pg.virtual_d = t->mm.code + (i * 265);
+            aloc = alloc();
+            if (aloc < 0)
+            {
+                remove_pgb(&(t->mm.pgb));
+                t->pid = 0;
+                return;
+            }
+            pg.physical_d = (unsigned int) aloc; // TODO: hace falta casting???
+            insert_pgb(&(t->mm.pgb), pg);
+        }
+
+        for (int i = 0; i < lines; i++)
+        {
+            set(i, rom[i + 16], &(t->mm.pgb));
+        }
+        t->life = lines; // TODO: preguntar iñaki!
+    }
+    else
+    {
+        printf("=> Error: fichero erroneo! \n");
+        exit(1);
+    }
+}
+
 void *process_generator(void *queue)
 {
-    print_info("Entrando al thread creador de procesos...", 0);
-    Queue *q;
-    q = (Queue*) queue;
-    pcb_struct new_pcb;
-    int pid = 2;
-    int gen_random_factor = 0;
-
-    new_pcb = generate_pcb(pid);
-    pid += 1;
-    new_pcb.quantum = QUANTUM;
-    q_insert(new_pcb, q);
-    printf("new_pid: %d, life_time: %d, quantum: %d, q_len: %d\n", new_pcb.pid, new_pcb.finishing_time, new_pcb.quantum, q->len);
-
-    while(1) {
-        gen_random_factor = rand() % 10;
-        sleep(1.0 + gen_random_factor); // entre 1-9s
-        new_pcb = generate_pcb(pid);
-        pid += 1;
-        new_pcb.quantum = QUANTUM;
-        q_insert(new_pcb, q);
-        printf("new_pid: %d, life_time: %d, quantum: %d, q_len: %d\n", new_pcb.pid, new_pcb.finishing_time, new_pcb.quantum, q->len);
-    }
+    FILE *f = fopen()
 }
